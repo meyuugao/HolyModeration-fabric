5bdd7c74fb12149707aa30adaf2d744aa9a0eb9c
Gr0wMan <bletsdone@gmail.com>
2025-12-10 17:50:18 +0300

    revert

--- src/main/java/me/yuugao/holymoderation/client/config/Config.java
+++ src/main/java/me/yuugao/holymoderation/client/config/Config.java
@@ -1,7 +1,5 @@
 package me.yuugao.holymoderation.client.config;
 
-import me.yuugao.holymoderation.client.obfuscation.DontObf;
-import me.yuugao.holymoderation.client.obfuscation.StringObfuscator;
 import org.apache.commons.lang3.StringUtils;
 import org.lwjgl.glfw.GLFW;
 
@@ -16,7 +14,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@DontObf
 public class Config {
     public Config() {
         keyBinds.put("open_main_gui", new KeyBindConfig(KeyBindType.SINGLE_PRESS, GLFW.GLFW_KEY_RIGHT_SHIFT));
@@ -26,14 +23,6 @@ public class Config {
 
     @Expose
     private String apiToken = StringUtils.EMPTY;
-    
-    public String getApiToken() {
-        return StringObfuscator.decrypt(apiToken);
-    }
-    
-    public void setApiToken(String token) {
-        this.apiToken = StringObfuscator.encrypt(token);
-    }
     @Expose
     private boolean soundsEnabled = true;
     @Expose
--- src/main/resources/assets/minecraft/shaders/core/rgb_palette.fsh
+++ src/main/resources/assets/minecraft/shaders/core/rgb_palette.fsh
@@ -21,23 +21,23 @@ void main() {
     vec2 center = Size * 0.5;
     vec2 pos = uv - center;
     float dist = length(pos);
-
+    
     if (dist > Radius + OutlineWidth) {
         discard;
     }
-
+    
     if (dist > Radius) {
         float t = (dist - Radius) / OutlineWidth;
         float alpha = 1.0 - smoothstep(0.0, 1.0, t);
         fragColor = vec4(OutlineColor.rgb, OutlineColor.a * alpha);
         return;
     }
-
+    
     float angle = atan(pos.y, pos.x);
     float hue = (angle + PI) / (2.0 * PI);
     float saturation = dist / Radius;
     float value = 1.0;
-
+    
     vec3 color = hsv2rgb(vec3(hue, saturation, value));
     fragColor = vec4(color, 1.0);
 }
