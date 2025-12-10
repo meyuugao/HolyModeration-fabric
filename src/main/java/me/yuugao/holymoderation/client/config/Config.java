8d23aa37bb3a26f29f0f780ea70929f234042a4f
Gr0wMan <bletsdone@gmail.com>
2025-12-10 17:52:02 +0300

    Revert "revert"
    This reverts commit 5bdd7c74fb12149707aa30adaf2d744aa9a0eb9c.

--- src/main/java/me/yuugao/holymoderation/client/config/Config.java
+++ src/main/java/me/yuugao/holymoderation/client/config/Config.java
@@ -1,5 +1,7 @@
 package me.yuugao.holymoderation.client.config;
 
+import me.yuugao.holymoderation.client.obfuscation.DontObf;
+import me.yuugao.holymoderation.client.obfuscation.StringObfuscator;
 import org.apache.commons.lang3.StringUtils;
 import org.lwjgl.glfw.GLFW;
 
@@ -14,6 +16,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@DontObf
 public class Config {
     public Config() {
         keyBinds.put("open_main_gui", new KeyBindConfig(KeyBindType.SINGLE_PRESS, GLFW.GLFW_KEY_RIGHT_SHIFT));
@@ -23,6 +26,14 @@ public class Config {
 
     @Expose
     private String apiToken = StringUtils.EMPTY;
+    
+    public String getApiToken() {
+        return StringObfuscator.decrypt(apiToken);
+    }
+    
+    public void setApiToken(String token) {
+        this.apiToken = StringObfuscator.encrypt(token);
+    }
     @Expose
     private boolean soundsEnabled = true;
     @Expose
