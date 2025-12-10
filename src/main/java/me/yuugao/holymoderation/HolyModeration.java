ad406a6badd25fa42161c450e9c2b4ab80013d18
Gr0wMan <bletsdone@gmail.com>
2025-12-10 17:52:17 +0300

    Revert "upd"
    This reverts commit 9cd6240e02f9db61a0bdf9ff318c131c5042bc59.

--- src/main/java/me/yuugao/holymoderation/HolyModeration.java
+++ src/main/java/me/yuugao/holymoderation/HolyModeration.java
@@ -1,12 +1,10 @@
 package me.yuugao.holymoderation;
 
-import me.yuugao.holymoderation.client.obfuscation.DontObf;
-
 import net.fabricmc.api.ModInitializer;
 
 public class HolyModeration implements ModInitializer {
+
     @Override
-    @DontObf
     public void onInitialize() {
     }
 }
