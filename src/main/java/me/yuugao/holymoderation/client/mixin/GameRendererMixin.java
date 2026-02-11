443c955aada77b7544194f780ee8506e9402ba8d
Gr0wMan <bletsdone@gmail.com>
2026-02-11 14:54:51 +0300

    upd
    gui fix

--- src/main/java/me/yuugao/holymoderation/client/mixin/GameRendererMixin.java
+++ src/main/java/me/yuugao/holymoderation/client/mixin/GameRendererMixin.java
@@ -4,10 +4,13 @@ import me.yuugao.holymoderation.client.eventbus.EventBus;
 import me.yuugao.holymoderation.client.eventbus.event.HudRenderEvent;
 import me.yuugao.holymoderation.client.util.serviceLocator.ServiceLocator;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.GameRenderer;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,18 +19,23 @@ import com.llamalad7.mixinextras.sugar.Local;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
+    @Shadow
+    @Final
+    MinecraftClient client;
+
     @Inject(
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;clear(IZ)V",
-                    shift = At.Shift.AFTER,
+                    target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V",
                     ordinal = 1
             )
     )
     private void onRender(float tickDelta, long startTime, boolean tick, CallbackInfo ci, @Local DrawContext drawContext) {
-        EventBus eventBus = ServiceLocator.getEventBus();
+        if (tick && client.world != null) {
+            EventBus eventBus = ServiceLocator.getEventBus();
 
-        eventBus.invokeEvent(new HudRenderEvent(drawContext, tickDelta));
+            eventBus.invokeEvent(new HudRenderEvent(drawContext, tickDelta));
+        }
     }
 }
---
+++ src/main/java/me/yuugao/holymoderation/client/mixin/SMixin.java
@@ -0,0 +1,19 @@
+package me.yuugao.holymoderation.client.mixin;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InventoryScreen.class)
+public class SMixin {
+    @Inject(method = "render", at = @At("HEAD"))
+    public void a(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        new Exception().printStackTrace();
+    }
+}
